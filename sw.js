e1d8bd405e4236a91007714a0a5741007eb9c0a2
Apple <Apple@Mac.local.tld>
2026-03-03 11:27:51 -0500

    Initial commit: Invoice Generator PWA
    Single-page invoice generator for painting jobs with PDF export,
    Outlook email integration, and offline PWA support.

---
+++ sw.js
@@ -0,0 +1,38 @@
+var CACHE_NAME = 'invoice-generator-v1';
+var URLS_TO_CACHE = [
+  './',
+  './index.html',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(names) {
+      return Promise.all(
+        names.filter(function(name) { return name !== CACHE_NAME; })
+             .map(function(name) { return caches.delete(name); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) return response;
+      return fetch(event.request).then(function(networkResponse) {
+        return networkResponse;
+      });
+    })
+  );
+});
